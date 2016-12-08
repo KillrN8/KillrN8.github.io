359c84fc09a17f0ab5ef2e185f62ee90a4773c27
KillrN8 <deadmau5boys@gmail.com>
2016-12-07 21:25:08 -0600

    Removed un-needed files
    Removed files that had been moved to main.js

--- click.js
+++
@@ -1,9 +0,0 @@
-var septim = 0;
-var x = 1;
-var sps = 0;
-var spc = 1;
-
-function sClick(x) { //Main Clicking Source
-		septim = septim + x;
-		document.getElementById("septims").innerHTML = septim.toFixed(0);
-	};
